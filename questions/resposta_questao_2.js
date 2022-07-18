8449e6cdee6755cd1ca09e7f426c6bee70f30701
Yasmin <yasmin.cga@gmail.com>
2022-07-18 12:15:25 -0300

    resposta questão 2

--- questions/resposta_questao_2.js
+++ questions/resposta_questao_2.js
@@ -1,1 +1,8 @@
 
+const numbers = [1, 2, 3, 2, 4, 5, 5, 6];
+
+// comparei o índice da primeira ocorrência de um elemento com todos os elementos Se eles não corresponderem estão duplicados
+const duplicates = numbers.filter((item, index) => index !== numbers.indexOf(item));
+
+console.log(duplicates);
+
