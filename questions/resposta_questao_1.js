b26883e00ae83627a53f7882e73ca2f3ac83fcd0
Yasmin <yasmin.cga@gmail.com>
2022-07-18 09:48:33 -0300

    inicio primeira questão

--- questions/resposta_questao_1.js
+++ questions/resposta_questao_1.js
@@ -1,1 +1,10 @@
+let numbers = [5, 13, 1, 44, 32, 15, 500]
+
+// menor para o maior
+let lowestToHighest = numbers.sort((a, b) => a - b);
+console.log(lowestToHighest)
+
+// maior para o menor
+let highestToLowest = numbers.sort((a, b) => b-a);
+console.log(highestToLowest)
 
