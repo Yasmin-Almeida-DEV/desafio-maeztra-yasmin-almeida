56d487b0136a181a2d8b79b3a38cc7f741902667
Yasmin <yasmin.cga@gmail.com>
2022-07-18 13:20:58 -0300

    Update resposta_questao_1.js

--- questions/resposta_questao_1.js
+++ questions/resposta_questao_1.js
@@ -1,14 +1,15 @@
-function estaOrdenado(nuns) {
-    for(let i = 1; i <= nuns.length-1; i++){
-        if(nuns[i] - nuns[i - 1] <= 0 ){
-            console.log(nuns[i])
-            console.log(nuns[i - 1])
+function estaOrdenado(nums) {
+    for(let i = 1; i <= nums.length-1; i++){
+        if(nums[i] - nums[i - 1] <= 0 ){
+            console.log(nums[i])
+            console.log(nums[i - 1])
             // compara se o numero anterior menos o atual é igual a 0
-            return console.log("não está ordenado")
+            return true
         }
     }
-    return console.log("está ordenado")
+    return false
 }
 
 console.log(estaOrdenado([2,1]))
-console.log(estaOrdenado([1,2]))
+console.log(estaOrdenado([1,1]))
+
